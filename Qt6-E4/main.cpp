3e063c5c3a1fec45c278cd99f5709cb59ab764c3
bluenight12 <ekehd258@gmail.com>
2024-02-02 17:50:26 +0900

    qt practice

--- Qt6-E4/main.cpp
+++ Qt6-E4/main.cpp
@@ -1,11 +1,30 @@
 #include <QCoreApplication>
 
 #include "test.h"
+#include "cat.h"
 
+//클래스의 복사는 허용되지 않는다
+void testing(Cat &cat){
+    qInfo() << "Address : " << &cat;
+}
+
+void testing2(Cat *cat){
+    qInfo() << "Address : " << cat;
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    Cat kitty;
+    kitty.setObjectName("Kitty");
+    testing(kitty);
+    testing2(&kitty);
+
+    Cat death;
+    death.setObjectName("Grim Reaper");
+    testing(death);
+    testing2(&death);
 
     Test test;
 
